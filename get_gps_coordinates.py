a4a3923701990393da5e5820a0a944ad38f57c6e
DariaLaz <daria.lazarova03@gmail.com>
2025-03-16 05:04:40 +0200

    fix get gps coordinates

--- get_gps_coordinates.py
+++ get_gps_coordinates.py
@@ -1,40 +1,50 @@
-import serial
 import time
+import os
+
+def get_line(file_path):
+    if not os.path.exists(file_path):
+        print("Error: GPS log file not found.")
+        return []
+
+    with open(file_path, "r") as file:
+        for line in file:
+            if line.startswith("$GNRMC"):
+                return line
 
 def get_gps_position():
-    gps_serial = serial.Serial("/dev/serial0", baudrate=9600, timeout=1)
-    
-    while True:
-        line = gps_serial.readline().decode('utf-8', errors='ignore').strip()
-        
-        if line.startswith("$GNRMC"): 
-            parts = line.split(",")
-            
-            if parts[2] == "A": # valid data
-                latitude = nmea_to_decimal(parts[3], parts[4])
-                longitude = nmea_to_decimal(parts[5], parts[6])
-                
-                return {
-                    "latitude": latitude,
-                    "longitude": longitude,
-                    "timestamp": parts[1]
-                }
+    os.system("sudo chmod 666 /dev/ttyS0")
+    line = get_line("/dev/ttyS0")
+    parts = line.split(",")
 
-        time.sleep(1)  # retry if no valid data is found
+    if parts[2] == "A": # valid data
+        latitude = nmea_to_decimal(parts[3], parts[4])
+        longitude = nmea_to_decimal(parts[5], parts[6])
+
+    return {
+        "latitude": latitude,
+        "longitude": longitude,
+        "timestamp": parts[1]
+    }
 
 # '4807.038' N -> 48.1173
 def nmea_to_decimal(value, direction):
     if not value:
         return None
 
-    degrees = int(value[:2])
-    minutes = float(value[2:]) / 60.0
+    if len(value.split(".")[0]) == 4:
+        degrees = int(value[:2])
+        minutes = float(value[2:]) / 60.0
+    else:
+        degrees = int(value[:3])
+        minutes = float(value[3:]) / 60.0
+
     decimal = degrees + minutes
 
-    if direction in ["S", "W"]:  
+    if direction in ["S", "W"]:
         decimal *= -1
 
     return round(decimal, 6)
+
 
 if __name__ == "__main__":
     position = get_gps_position()
